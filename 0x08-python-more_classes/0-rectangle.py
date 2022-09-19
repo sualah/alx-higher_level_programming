9655cb5c09606486d0b684894f09f2f0ac148cc0
sualah <sabdullai@acsint.io>
2022-09-19 13:43:03 +0000

    chore(app):python more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 19 2022
+@author: Salifu Abdullai
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
