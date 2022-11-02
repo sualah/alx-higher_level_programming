25d4498e082bd55e3045a669302a868554792cd1
root <sualahabdullai@gmail.com>
2022-11-02 17:36:36 +0000

    create first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- create first_table inside current database
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT AUTO_INCREMENT,
-	name VARCHAR(256),
-	);
+CREATE TABLE IF NOT EXISTS first_table (id INT,name VARCHAR(256));
