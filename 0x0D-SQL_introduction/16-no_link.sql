045ab50de3c75579a2524dba669da840058b16bb
root <sualahabdullai@gmail.com>
2022-11-02 20:21:58 +0000

    where name is not empty

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 --- list all records of the second_table
-SELECT score, name FROM second_table WHERE name != '' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
