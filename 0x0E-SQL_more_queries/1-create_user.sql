cfb93dea609b39917d7b64ff79b44d42cbffcbe5
root <sualahabdullai@gmail.com>
2022-11-03 22:38:57 +0000

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
--- Creates the user user_0d_1
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Create an user with password
+-- DDL query to create a MySQL user with pasword
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- add privileges to user
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
